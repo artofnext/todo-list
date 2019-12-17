6a34cc6bcdc50887c1658a2089be81680c370444
artofnext <novnovalex@gmail.com>
2019-12-17 18:18:33 +0200

    worked simple version

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -5,11 +5,22 @@
             <input v-model="todo" type="text" placeholder="Type toto here...">
             <button v-on:click.prevent="setTodo(todo)" type="submit">Add!</button>
         </form>
+
+            <div>
+                <h3>Here should be the list</h3>
+                    <ol>
+                        <li v-for="(value, key) in todoArray" :key="key">{{ value }}
+                            <button v-on:click.prevent="todoArray.splice(key, 1)">x</button>
+                        </li>
+                    </ol>
+            </div>
+            <h2>Total todos: {{ todoArray.length }}</h2>
     </div>
 </template>
 
 <script>
-// import { watch } from 'fs';
+// import ListTodo from '@/components/ListTodo';
+
 export default {
     name: 'Form',
     data() {
@@ -18,12 +29,32 @@ export default {
             todoArray: [],
         }
     },
+    components: {
+        // ListTodo,
+    },
     methods: {
         setTodo: function(todo) {
             this.todoArray.push(todo);
             this.todo = "";
             console.log("Todos: " + this.todoArray);
+        },
+        removeTodo: function(event) {
+            console.log('Pressed');
+            console.log(event);
+        }
+        
+    },
+    watch: {
+        todoArray() {
+            console.log('todoArray: ' + this.todoArray);
         }
     },
 }
 </script>
+
+<style lang="scss" scoped>
+    ol {
+        margin: 0 auto;
+        max-width: 500px;
+    }
+</style>
---
+++ src/components/ListTodo.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    <h3>Here should be the list</h3>
+    <ol>
+        <li v-for="(value, key) in todoArray" :key="key">{{ value }}<button v-on:click="remove">x</button></li>
+    </ol>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ListTodo",
+    data() {
+
+        console.log('Here!')
+        return {
+            todoArray: [],
+        }
+    },
+    
+}
+</script>
