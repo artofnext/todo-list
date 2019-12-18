a07d0e8ae65b9cb3f3c9af135679c31af7470203
artofnext <novnovalex@gmail.com>
2019-12-18 16:34:11 +0200

    add element remove, animation

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,18 +1,19 @@
 <template>
     <div class="form-container">
         <form class="todo-form" name="todo">
-            <h2>{{ todo }}</h2>
+            <!-- <h2>{{ todo }}</h2> -->
             <input v-model="todo" type="text" placeholder="Type toto here...">
             <button v-on:click.prevent="setTodo(todo)" type="submit">Add!</button>
         </form>
-
             <div>
-                <h3>Here should be the list</h3>
-                    <ol>
-                        <li v-for="(value, key) in todoArray" :key="key">{{ value }}
-                            <button v-on:click.prevent="todoArray.splice(key, 1)">x</button>
+                <h3  v-if="todoArray.length > 0">You have to do:</h3>
+                    <transition-group name="list" tag="ol">
+                    <!-- <ol> -->
+                        <li v-for="(value, key) in todoArray" :key="key + 'element'" :class="'list-item ' + ((key % 2) ? 'even' : 'odd')">{{ value }}
+                            <button class="close rounded black" v-on:click.prevent="removeTodo(key)"></button>
                         </li>
-                    </ol>
+                    <!-- </ol> -->
+                    </transition-group>
             </div>
             <h2>Total todos: {{ todoArray.length }}</h2>
     </div>
@@ -25,6 +26,7 @@ export default {
     name: 'Form',
     data() {
         return {
+            key: 0,
             todo: "",
             todoArray: [],
         }
@@ -38,9 +40,17 @@ export default {
             this.todo = "";
             console.log("Todos: " + this.todoArray);
         },
-        removeTodo: function(event) {
-            console.log('Pressed');
-            console.log(event);
+        removeTodo: function(key) {
+
+            let self = this;
+
+            setTimeout(function () { // for purpose to pass param to callback function
+                removeElem(key);
+                }, 1000);
+
+            function removeElem (key) {
+                self.todoArray.splice(key, 1);
+            }
         }
         
     },
@@ -53,8 +63,92 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    $color-list-background: #eee;
+
     ol {
         margin: 0 auto;
         max-width: 500px;
+        text-align: left;
+        font-size: 20px;
+        line-height: 30px;
+
+        .odd {
+            background-color: $color-list-background;
+        }
+        
+        .even {
+            background-color: lighten( $color-list-background, 20%);
+        }
+
+        .list-item {
+            // display: inline-block;
+            // margin-right: 10px;
+        }
+        .list-enter-active, .list-leave-active {
+            transition: all 1s;
+        }
+        .list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+            opacity: 0;
+            transform: translateX(30px);
+        }
+        .list-move {
+            transition: transform 1s;
+        }
+
+
+        li {
+            width: 450px;
+            & button {
+                float: right;
+                border: none;
+                margin: 7px;
+                background-color: transparent;
+            }
+        }
+
+        .close{
+            position: relative;
+            display: inline-block;
+            width: 15px;
+            height: 15px;
+            overflow: hidden;
+            &:hover {
+                &::before, &::after {
+                    background: blue;
+                }
+            }
+
+            &::before, &::after {
+              content: '';
+              position: absolute;
+              height: 2px;
+              width: 100%;
+              top: 50%;
+              left: 0;
+              margin-top: -1px;
+              background: #000;
+            }
+
+            &::before {
+                transform: rotate(45deg);
+            }
+
+            &::after {
+                transform: rotate(-45deg);
+            }
+
+            .rounded {
+                    &::before, &::after {
+                        border-radius: 5px;
+                    }
+                }
+
+            .black {
+                    &::before, &::after {
+                        height: 8px;
+                        margin-top: -4px;
+                    }
+                }
+        }
     }
 </style>
