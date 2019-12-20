2cf4205b24b33b3b37a5c91beb0fc6c9598d1849
artofnext <novnovalex@gmail.com>
2019-12-20 10:57:25 +0200

    add styles and behavier to modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="modal">
-        <div class="modal__container">
+    <div class="modal"  v-on:click="modalTitle = 'Close!'" >
+        <div class="modal__container" v-on:click.stop>
             <h2 class="modal__title">{{ modalTitle }}</h2>
             <CloseButton class="list-close" v-on:click.native="modalTitle = 'Close!'" />
 
@@ -21,3 +21,25 @@ export default {
     },
 }
 </script>
+
+<style lang="scss" scoped>
+    .modal {
+        position: absolute;
+        background-color: rgba($color: #000000, $alpha: .5);
+        width: 100vw;
+        height: 100vh;
+        z-index: 10;
+    }
+
+    .modal__container {
+        position: absolute;
+        left: 50%;
+        right: 50%;
+        background-color: white;
+        width: 30%;
+        height: 30%;
+        transform: translate(-50%, 50%);
+        z-index: 20;
+
+    }
+</style>
