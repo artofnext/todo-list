68e8a5e53dae3ad67f22e29117cd2677ede57b5b
artofnext <novnovalex@gmail.com>
2019-12-20 09:59:51 +0200

    add modal template

---
+++ src/components/Modal.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="modal">
+        <div class="modal__container">
+            <h2 class="modal__title">{{ title }}</h2>
+            
+        </div>
+    </div>
+</template>
