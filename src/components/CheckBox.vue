6d6e85ccb9db493f527c22ba80f6f41876d3eb38
artofnext <novnovalex@gmail.com>
2019-12-27 18:05:22 +0200

    add checkbox component

---
+++ src/components/CheckBox.vue
@@ -0,0 +1,43 @@
+<template>
+    <div 
+    class="check-outer" 
+    :class="{ checked: checked, disabled: disabled }"
+    @click="changeState()"
+    >
+        <div class="check-inner"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Ceckbox",
+    props: {
+        checked: {
+            type: Boolean,
+            required: true,
+        },
+
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    methods: {
+        changeState() {
+            this.$emit("changed", !this.checked);
+        },
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    $color-dark-main: #2c3e50;
+
+    .check-outer {
+
+    }
+
+    .check-inner {
+
+    }
+</style>
